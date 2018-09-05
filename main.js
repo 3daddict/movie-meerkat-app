7728dbe0306884077c151694e3e8d50c250ed7fe
valor323 <caine101@mail.chapman.edu>
2018-09-05 09:33:27 -0700

    add js skeleton

---
+++ main.js
@@ -0,0 +1,33 @@
+document.ready(initializeApp)
+
+
+function initializeApp(){
+
+    
+}
+
+
+
+
+//has ajax paramaters
+//@calls ajax new york times
+
+function newYorkTimesAjax (){
+
+}
+
+//what to do if ajax fires successfully
+//finds the synopsis of the movie and appends it to the trailer
+//finds the link to the whole review and appends it as well
+
+function newYorkTimesAjaxSuccessful(){
+
+}
+
+
+
+//what to do if it ajax gets error from server
+//instead of appending synospsis and link to review apeends tecx that says they are unavailable at this time
+function newYorkTimesAjaxError(){
+
+}
