565350d50a6a831ae6e29143839cb5563dcbd163
Michael Salvati <print@3daddict.com>
2018-09-05 12:20:15 -0700

    created movie dom elements function movieListingsOnDOM

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ $(document).ready(initializeApp)
 * initializes the application, including adding click handlers and pulling in any data from the server, in later versions*/
 function initializeApp(){
     newYorkTimesAjax();
-    
+    movieListingsOnDOM(); //appends movies to the dom
 }
 
-
+//Global Variables
+var movieListings = [];
 
 
 //has ajax paramaters
@@ -41,8 +42,8 @@ function newYorkTimesAjaxSuccessful(responseData){
     console.log("responseData:", responseData);
     var items = responseData.results
     var summary = items.map(items => $('<div>').text(items.summary_short));
-    var linkToReview = items.map(items => $('<a>').text(items.link.url).attr('href', items.link.url));
-    $('body').append(summary, linkToReview);
+    //var linkToReview = items.map(items => $('<a>').text(items.link.url).attr('href', items.link.url));
+    //$('body').append(summary, linkToReview);
 }
 
 
@@ -55,7 +56,7 @@ function newYorkTimesAjaxError(){
 
 
 
-// * initializeApp
+// * TMDB Ajax Function
 // * @param  {} settings
 // * @param  {} .done(function(response))
 // * @returns: {response} 
@@ -72,9 +73,31 @@ var settings = {
   }
   
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    //console.log(response);
+    movieListings.push(response);
   });
 
+// * movieListingsOnDOM Function
+// * @param  {} none 
+// * @returns: {} none 
+// * appends movieListings to the DOM*/
+function movieListingsOnDOM(){
     
-}
+    for(var i = 0; i < movieListings[0].results.length; i++){
+        var movieTitle = movieListings[0].results[i].title;
+        var moviePoster = movieListings[0].results[i].poster_path;
+        var movieRating = movieListings[0].results[i].vote_average;
 
+        var addMovieRow = $('<div>').addClass('movieRow');
+        var addMoviePoster = $('<img>').attr('src', 'http://image.tmdb.org/t/p/w185' + moviePoster);
+        var addMovieContainer = $('<div>').addClass('movieCardInfo');
+        var addMovieTitle = $('<p>').addClass('movieTitle ');
+        addMovieTitle.append(movieTitle);
+        var addMovieRating = $('<p>').addClass('movieRating');
+        addMovieRating.append(movieRating);
+        addMovieContainer.append(addMovieTitle, addMovieRating);
+
+        $(".movie-container").append(addMovieRow);
+        addMovieRow.append(addMoviePoster, addMovieContainer);
+    }
+}
