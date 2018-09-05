72e5f931402d3804f1e00d0e196e882b507c96df
valor323 <caine101@mail.chapman.edu>
2018-09-05 15:48:51 -0700

    added movie title param to nyt ajax call and added click handler to wipe page

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function initializeApp(){
     newYorkTimesAjax();
     movieListingsOnDOM(); //appends movies to the dom
     addressCoordinates();
+    $('.movieRow').on('click', clickHandlerToOpenNewPage)
 }
 
 //Global Variables
@@ -14,21 +15,21 @@ var movieListings = [];
 //has ajax paramaters
 //@calls ajax new york times
 //@params called query value is movie title
-function newYorkTimesAjax (){
-    var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-    url += '?' + $.param({
-      'api-key': "8f55164da30c48c9ba4dc79d9fce1827"
-    });
-    $.ajax({
-      url: url,
+function newYorkTimesAjax (movieTitle){
+    var newYorkTimesParams = {
+      url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json",
       method: 'GET',
+      data: {
+        'api-key': "8f55164da30c48c9ba4dc79d9fce1827",
+          "query": movieTitle,
+      },
       success: newYorkTimesAjaxSuccessful,
       error: newYorkTimesAjaxError,
-    })
+    }
     
     
 
-    $.ajax( url);
+    $.ajax( newYorkTimesParams );
 }
 
 //what to do if ajax fires successfully
@@ -37,10 +38,10 @@ function newYorkTimesAjax (){
 
 function newYorkTimesAjaxSuccessful(responseData){
     console.log("responseData:", responseData);
-    var items = responseData.results
-    var summary = items.map(items => $('<div>').text(items.summary_short));
-    //var linkToReview = items.map(items => $('<a>').text(items.link.url).attr('href', items.link.url));
-    //$('body').append(summary, linkToReview);
+    var linkToReview = $('<div>').text(responseData.results[0].summary_short);
+    var summary = $('<a>').text(responseData.results[0].link.url).attr('href', responseData.results[0].link.url)
+    $('body').append(summary, linkToReview);
+  
 }
 
 
@@ -84,11 +85,7 @@ function movieListingsOnDOM(){
         var movieTitle = movieListings[0].results[i].title;
         var moviePoster = movieListings[0].results[i].poster_path;
         var movieRating = movieListings[0].results[i].vote_average;
-
-        var addMovieRow = $('<div>').addClass('movieRow');
-        $('.movieRow').on('click', function(){
-          location.href = "movie.html"
-        })
+        var addMovieRow = $('<div>').addClass('movieRow').attr('data-title', movieTitle);
         var addMoviePoster = $('<img>').attr('src', 'http://image.tmdb.org/t/p/w185' + moviePoster);
         var addMovieContainer = $('<div>').addClass('movieCardInfo');
         var addMovieTitle = $('<p>').addClass('movieTitle ');
@@ -96,7 +93,6 @@ function movieListingsOnDOM(){
         var addMovieRating = $('<p>').addClass('movieRating');
         addMovieRating.append(movieRating);
         addMovieContainer.append(addMovieTitle, addMovieRating);
-
         $(".movie-container").append(addMovieRow);
         addMovieRow.append(addMoviePoster, addMovieContainer);
     }
@@ -148,18 +144,24 @@ function failedYelpCall(){
 }
 
 
-function addresaddedsCoordinates(){
+function addressCoordinates(){
     var ajaxParams = {
         url: "https://api.opencagedata.com/geocode/v1/json",
         data: {
             key: "52645efc693e4815825c94314f6d5f77",
-            q: "columbus, Ohio"
+            q: "columbus, Ohio",
         },
         success: successfullAddressCoordinates
-        
     }  
-  $.ajax(ajaxParams);
+    $.ajax(ajaxParams)
 }
 function successfullAddressCoordinates(responseCoordinates){
     console.log('responseCoordinates', responseCoordinates);
+}
+
+
+function clickHandlerToOpenNewPage (){
+  console.log($(this).attr('data-title'))
+  $('body').empty();
+  newYorkTimesAjax($(this).attr('data-title'))
 }
