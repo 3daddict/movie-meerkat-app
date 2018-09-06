5086c942975c659dce3a863903f958789f7270da
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-09-06 16:52:53 -0700

    fixed loop to place map markers

--- main.js
+++ main.js
@@ -200,6 +200,7 @@ function successfulYelpCall(response){
     console.log('Rating :',response.businesses[0]['rating']);
     console.log('Review :',response.businesses[0]['review_count']);
     yelpResult = response.businesses;
+    initMap();
 }
 
 /****************************************************************************************************
@@ -269,136 +270,39 @@ function successfullAddressCoordinates(responseCoordinates){
     
 }
 
-
-
-
-// var map;
-//       function initMap(lat,lng) {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 8,
-//           center: new google.maps.LatLng(lat,lng),
-//         //   mapTypeId: 'terrain',
-//         });
-
-//         // Create a <script> tag and set the USGS URL as the source.
-//         var script = document.createElement('script');
-//         // This example uses a local copy of the GeoJSON stored at
-//         // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//         script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//         document.getElementsByTagName('head')[0].appendChild(script);
-//       }
-
-//       // Loop through the results array and place a marker for each
-//       // set of coordinates.
-//       window.eqfeed_callback = function(results) {
-//         for (var i = 0; i < results.features.length; i++) {
-//           var coords = results.features[i].geometry.coordinates;
-//           var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//           var marker = new google.maps.Marker({
-//             position: latLng,
-//             map: map
-//           });
-//         }
-      
-//       }
-// var map;
-// function initMap(lat,lng) {
-//     var movieTheaters = {lat: lat, lng: lng};
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//       zoom: 8,
-//       center: {lat: lat , lng: lng},
-//     });
-  
-//     var contentString = '<div id="content">'+
-//         '<div id="siteNotice">'+
-//         '</div>'+
-//         '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-//         '<div id="bodyContent">'+
-//         '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-//         'sandstone rock formation in the southern part of the '+
-//         'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-//         'south west of the nearest large town, Alice Springs; 450&#160;km '+
-//         '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-//         'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-//         'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-//         'Aboriginal people of the area. It has many springs, waterholes, '+
-//         'rock caves and ancient paintings. Uluru is listed as a World '+
-//         'Heritage Site.</p>'+
-//         '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-//         'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-//         '(last visited June 22, 2009).</p>'+
-//         '</div>'+
-//         '</div>';
-  
-//     var infowindow = new google.maps.InfoWindow({
-//       content: contentString,
-//       maxWidth: 300
-//     });
-  
-//     var marker = new google.maps.Marker({
-//       position: movieTheaters,
-//       map: map,
-//       title: 'Uluru (Ayers Rock)'
-//     });
-//     marker.addListener('click', function() {
-//       infowindow.open(map, marker);
-//     });
-//   }
 var map;
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var sydney = {lat: -33.8688, lng: 151.2093};
+    var learningFuze = {lat: 33.634857, lng: -117.74044};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+        zoom: 11,
+        center: learningFuze
     });
 
     var contentString = '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<h1 id="firstHeading" class="firstHeading">Theater Name</h1>' +
         '<div id="bodyContent">' +
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the ' +
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-        'Aboriginal people of the area. It has many springs, waterholes, ' +
-        'rock caves and ancient paintings. Uluru is listed as a World ' +
-        'Heritage Site.</p>' +
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-        '(last visited June 22, 2009).</p>' +
         '</div>' +
         '</div>';
 
     var infowindow = new google.maps.InfoWindow({
-        content: contentString
+        content: contentString,
+        maxWidth: 250,
+        maxHeight: 100
     });
 
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: learningFuze,
         map: map,
-        title: 'Uluru (Ayers Rock)'
-    });
-
-    var marker2 = new google.maps.Marker({
-        position: sydney,
-        map: map,
-        title: 'Sydney'
+        title: 'LearningFuze'
     });
 
     marker.addListener('click', function () {
         infowindow.open(map, marker);
     });
 
-    marker2.addListener('click', function () {
-        infowindow.open(map, marker2);
-    });
-
-    for(var i = 0; i < yelpResult.length; i++){
+    for(var i = 0; i < 10; i++){
         var position = {lat: yelpResult[i]['coordinates']['latitude'], lng: yelpResult[i]['coordinates']['longitude']};
         var newMarker = new google.maps.Marker({
             position: position,
